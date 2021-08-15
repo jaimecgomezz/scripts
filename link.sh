b101cb461cfe27dd7b264fc372543cf1c42eb25a
jaimecgomezz <jaimecgomezwork@gmail.com>
2021-08-15 10:56:21 -0500

    feat: Add the link script

---
+++ link.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+##############################
+# @jaimecgomezz
+#
+# exposes scripts contained
+# in folder's subfolders
+##############################
+
+FILE_NAME="$( basename "$0" )"
+EXPOSED_PATH="$( realpath "$0" | sed "s|$FILE_NAME||g" )"
+
+cd "$EXPOSED_PATH"
+
+find . -maxdepth 1 -type l -delete
+
+for f in */*.sh ; do
+  name="$( basename "$f" | sed 's/\.sh//' )"
+
+  ln -s "${EXPOSED_PATH}${f}" "${EXPOSED_PATH}${name}"
+done
