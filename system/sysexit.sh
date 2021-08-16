81248d8b73048e6ec6202a7980ac3b88c2f62130
jaimecgomezz <jaimecgomezwork@gmail.com>
2021-08-16 03:02:20 -0500

    feat: Add systems's base scripts

---
+++ system/sysexit.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+##############################
+# @jaimecgomezz
+#
+# system exit management
+####################### global
+DMENU="${DMENU:-dmenu}"
+####################### script
+EXIT_MANAGER=i3exit
+##############################
+
+function get_selection() {
+  msg="$1"; shift
+  options=("$@")
+
+  selection="$( printf '%s\n' "${options[@]}" | ${DMENU} -p "$msg" -auto-select -filter '[' -matching normal )"
+
+  echo "$selection"
+
+  [ -z "$selection" ] && return 1 || return 0
+}
+
+actions=('[l]ock' '[s]uspend' '[h]ibernate' '[r]eboot' '[S]hutdown')
+action="$( get_selection 'actions' "${actions[@]}" )"
+
+[ ! -z "$action" ] || exit 1
+
+case "$action" in
+  '[l]ock'      ) ${EXIT_MANAGER} 'lock'       ;;
+  '[s]uspend'   ) ${EXIT_MANAGER} 'suspend'    ;;
+  '[h]ibernate' ) ${EXIT_MANAGER} 'hibernate'  ;;
+  '[r]eboot'    ) ${EXIT_MANAGER} 'reboot'     ;;
+  '[S]hutdown'  ) ${EXIT_MANAGER} 'shutdown'   ;;
+esac
