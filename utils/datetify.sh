767796b1e238ce3d0423a06baeb436219e4825b2
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-02-14 17:40:24 -0600

    feat: Add datetify and setkb scripts

---
+++ utils/datetify.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+##############################
+# @jaimecgomezz
+#
+# shows date as notification
+#
+##############################
+
+notify-send -t 10000 "date" "$( date)"
---
+++ utils/setkb.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+##############################
+# @jaimecgomezz
+#
+# sets appropiate keyboard
+#
+##############################
+
+setxkbmap us -variant altgr-intl -option 'ctrl:nocaps'
