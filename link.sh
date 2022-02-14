c477e435ad7920e7c6f7a6b4045b903b0d323972
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-02-14 17:41:05 -0600

    refactor: Format link script

--- link.sh
+++ link.sh
@@ -7,25 +7,27 @@
 # in folder's subfolders
 ####################### script
 IGNORED=(
+  "tags"
   "utils/hosts/*"
+  "dcompose/workspaces"
 )
-FILE_NAME="$( basename "$0" )"
-EXPOSED_PATH="$( realpath "$0" | sed "s|$FILE_NAME||g" )"
+FILE_NAME="$( basename "$0")"
+EXPOSED_PATH="$( realpath "$0" | sed "s|$FILE_NAME||g")"
 ##############################
 
-cat /dev/null > .gitignore
+cat /dev/null >.gitignore
 for i in "${IGNORED[@]}"; do
-  echo "$i" >> .gitignore
+  echo "$i" >>.gitignore
 done
 
 cd "$EXPOSED_PATH" || exit 1
 
 find . -maxdepth 1 -type l -delete
 
-for f in */*.sh ; do
-  name="$( basename "$f" | sed 's/\.sh//' )"
+for f in */*.sh; do
+  name="$( basename "$f" | sed 's/\.sh//')"
 
   ln -s "${EXPOSED_PATH}${f}" "${EXPOSED_PATH}${name}"
 
-  echo "$name" >> .gitignore
+  echo "$name" >>.gitignore
 done
