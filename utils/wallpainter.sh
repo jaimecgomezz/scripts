5541d9070ac753283cc5241158888df711e4622b
jaimecgomezz <jaimecgomezwork@gmail.com>
2022-04-15 09:21:25 -0500

    refactor: Improve wallpainter.sh

--- utils/wallpainter.sh
+++ utils/wallpainter.sh
@@ -21,40 +21,40 @@
 ####################### global
 DMENU=${DMENU:-dmenu}
 ####################### script
-THEME=nord
+THEME=dark
 WALLPAPERS=~/pictures/walls
-SELF="$( basename "$0" | sed 's/\.sh//g')"
+SELF="$(basename "$0" | sed 's/\.sh//g')"
 ##############################
 
 PREVIOUS="$1"
 
 get_selection() {
-  msg="$1"
-  shift
-  options=("$@")
+	msg="$1"
+	shift
+	options=("$@")
 
-  selection="$( printf '%s\n' "${options[@]}" | ${DMENU} -p "$msg" -select "$PREVIOUS")"
+	selection="$(printf '%s\n' "${options[@]}" | ${DMENU} -p "$msg" -select "$PREVIOUS")"
 
-  echo "$selection"
+	echo "$selection"
 
-  [ -z "$selection" ] && return 1 || return 0
+	[ -z "$selection" ] && return 1 || return 0
 }
 
 set_wallpaper() {
-  ln -sf "$WALLPAPERS/$THEME/$1" "$WALLPAPERS/wallpaper"
-  hsetroot -cover "$WALLPAPERS/wallpaper"
-  notify-send "$SELF" "$1"
+	ln -sf "$WALLPAPERS/$THEME/$1" "$WALLPAPERS/wallpaper"
+	hsetroot -cover "$WALLPAPERS/wallpaper"
+	notify-send "$SELF" "$1"
 }
 
-found="$(  ls -l "$WALLPAPERS/$THEME" | grep -e jpg -e png | awk '{print $9}')"
+found="$(ls -l "$WALLPAPERS/$THEME" | grep -e jpg -e png | awk '{print $9}')"
 wallpapers=(done "${found[@]}")
-wallpaper="$( get_selection 'wallpaper' "${wallpapers[@]}")"
+wallpaper="$(get_selection 'wallpaper' "${wallpapers[@]}")"
 
 [ "$?" = 0 ] || exit 1
 
 case "$wallpaper" in
-  "done") exit 0                ;;
-  *) set_wallpaper "$wallpaper" ;;
+"done") exit 0 ;;
+*) set_wallpaper "$wallpaper" ;;
 esac
 
 "$SELF" "$wallpaper"
